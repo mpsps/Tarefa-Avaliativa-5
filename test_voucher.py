42055e3a9c64d807b8f8f73ebe06fa6d7eaf9ff5
mpsps <55263599+mpsps@users.noreply.github.com>
2025-08-11 21:54:03 -0300

    testado

--- test_voucher.py
+++ test_voucher.py
@@ -1,21 +1,21 @@
 import pytest
-from app.voucher import validar_voucher
+from voucher import validar_voucher
 
 @pytest.mark.parametrize("valor", [
-    50.00,     # limite inferior
-    500.00,    # limite superior
-    50.01,     # logo acima do limite inferior
-    499.99,    # logo abaixo do limite superior
-    275.50     # valor médio
+    50.00,    
+    500.00,    
+    50.01,     
+    499.99,    
+    275.50     
 ])
 def test_valor_valido(valor):
     assert validar_voucher(valor) is True
 
 @pytest.mark.parametrize("valor", [
-    49.99,     # logo abaixo do limite inferior
-    0.00,      # extremo inferior
-    500.01,    # logo acima do limite superior
-    1000.00    # extremo superior
+    49.99,     
+    0.00,      
+    500.01,    
+    1000.00  
 ])
 def test_valor_invalido(valor):
     assert validar_voucher(valor) is False
